8ad4b329447ce5e1a904ae6421455333d09a7bd6
Saref111 <saref012@gmail.com>
2020-01-27 16:42:35 +0800

    Fix for eslint

--- js/stat.js
+++ js/stat.js
@@ -6,17 +6,16 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 50;
-var TEXT_WIDTH = 50;
 var MAX_BAR_HEIGHT = 150; // barHeight
 var BAR_WIDTH = 40;
 var myColor = 'rgba(255, 0, 0, 1)';
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -28,7 +27,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -47,7 +46,7 @@ window.renderStatistics = function(ctx, players, times) {
 
     ctx.fillText(players[i], CLOUD_X + FONT_GAP + (FONT_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
 
-    if (players[i] == 'Вы') {
+    if (players[i] === 'Вы') {
       ctx.fillStyle = myColor;
     } else {
       var saturation = Math.floor(Math.random() * 100);
