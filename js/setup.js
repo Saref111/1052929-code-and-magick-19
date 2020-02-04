1a3dee96faa3f141008929a3e3abbca42999a1cc
Saref111 <saref012@gmail.com>
2020-02-04 20:04:21 +0800

    Added showModal function

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+var showModal = function () {
+  userDialog.classList.remove('hidden');
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+}
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -51,4 +53,5 @@ var drawTemplates = function () {
   similarListElement.appendChild(fragment);
 };
 
+showModal();
 drawTemplates();
